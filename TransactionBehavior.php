1fba5475532f1f9099ba22b1411e43db4de891d1
Ekstazi <ekstazyst@gmail.com>
2013-02-21 20:47:09 +0200

    Transaction behavior v1.0

--- TransactionBehavior.php
+++ TransactionBehavior.php
@@ -6,50 +6,95 @@
  * Time: 16:40
  * To change this template use File | Settings | File Templates.
  */
+/**
+ * This behavior allow to use transaction for model between onBeforeSave and onAfterSave events
+ */
 class TransactionBehavior extends CActiveRecordBehavior
 {
+	/**
+	 * Whether to autostart transaction on save.
+	 * Note you must do your additional operations after onBeforeSave and before onAfterSave events
+	 * @var bool
+	 */
 	public $autoStart=false;
 
+	/**
+	 * @var CDbTransaction
+	 */
 	protected $_transaction;
 
+	/**
+	 * Start the transaction
+	 * @return CDbTransaction
+	 */
 	public function beginTransaction()
 	{
-		if($this->_transaction)
-			throw new CException('Already in transaction');
 		$db = $this->owner->dbConnection;
-		if($db->currentTransaction)
-			return $db->currentTransaction;
+		if($db->currentTransaction||$this->_transaction)
+			throw new CDbException(Yii::t('transaction','Already in transaction'));
+
 
 		Yii::app()->attachEventHandler('onException',array($this,'rollback'));
-		$this->_transaction=$db->beginTransaction();
+		return $this->_transaction=$db->beginTransaction();
 	}
 
+	/**
+	 * Rollback transaction
+	 */
 	public function rollback()
 	{
 		Yii::app()->detachEventHandler('onException',array($this,'rollback'));
-		if($this->_transaction)
-			$this->_transaction->rollback();
+		if(!$this->_transaction)
+			throw new CDbException(Yii::t('transaction','Nothing to rollback'));
+		$this->_transaction->rollback();
 		unset($this->_transaction);
 	}
 
+	/**
+	 * Commit transaction
+	 */
 	public function commit()
 	{
-		Yii::app()->detachEventHandler('onException',array($this,'rollback'));
+		if(!$this->_transaction)
+			throw new CDbException(Yii::t('transaction','Nothing to commit'));
+
 		$this->_transaction->commit();
 		unset($this->_transaction);
+		Yii::app()->detachEventHandler('onException',array($this,'rollback'));
 	}
 
 	public function beforeSave($event)
 	{
 		if($this->autoStart)
 			$this->beginTransaction();
-		parent::beforeSave($event);
 	}
 
 	public function afterSave($event)
 	{
 		if($this->autoStart&&$this->_transaction)
 			$this->commit();
-		parent::afterSave($event);
+	}
+
+	/**
+	 * Save model with transaction handle
+	 * @param bool $runValidation
+	 * @param null $attributes
+	 * @return bool Whether the transaction save was successful
+	 */
+	public function saveTransactional($runValidation=true,$attributes=null)
+	{
+		$autoStart=$this->autoStart;
+		$this->autoStart=false;
+		try {
+			$this->beginTransaction();
+			$this->owner->save($runValidation,$attributes);
+		}catch (Exception $e)
+		{
+			$this->rollback();
+			return false;
+		}
+		$this->commit();
+		$this->autoStart=$autoStart;
+		return true;
 	}
 }
